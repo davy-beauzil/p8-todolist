8b323b225fb94f6abc9b7b592e305dac35981c3b
Davy <davy.beauzil72@gmail.com>
2023-03-23 17:48:28 +0100

    test with Authenticator

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,32 +10,27 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="login")
+     * @Route("/login", name="app_login")
      */
-    public function loginAction(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error,
-        ]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-//    /**
-//     * @Route("/login_check", name="login_check")
-//     */
-//    public function loginCheck(): void
-//    {
-//        // This code is never executed.
-//    }
-//
     /**
-     * @Route("/logout", name="logout")
+     * @Route("/logout", name="app_logout")
      */
-    public function logoutCheck(): void
+    public function logout(): void
     {
-        // This code is never executed.
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
